2e1af0d06fafcbe64079e83dbb5c9db8fbc29bc9
valosekj <jan.valosek@upol.cz>
2020-09-04 11:05:34 +0300

    added usage

--- scripts/extract_normative_metrics_wrapper.sh
+++ scripts/extract_normative_metrics_wrapper.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # This is wrapper for extract_normative_metrics.py to allow parallel run using SCT function sct_run_batch
+#
+# USAGE (using SCT sct_run_batch function):
+# 	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1 -script scripts/extract_normative_metrics_wrapper.sh
+#
 
 # Variables $1 and $PATH_RESULTS are passed from sct_run_batch script
 SUBJECT=$1
 PATH_DATA=$(echo $PATH_RESULTS | sed 's|\(.*\)/.*|\1|')		# delete "/results" from the end of PATH_RESULTS variable
 
-echo $SUBJECT
-echo $PATH_DATA
-
-python extract_normative_metrics.py -sub ${SUBJECT} -path-data ${PATH_DATA}
+python /usr/local/normative-metrics/scripts/extract_normative_metrics.py -sub ${SUBJECT} -path-data ${PATH_DATA}
