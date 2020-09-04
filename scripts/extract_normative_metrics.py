9460e86ecfdcd6a95ddb8aabf83a08df4a9c0ed2
valosekj <jan.valosek@upol.cz>
2020-09-04 11:44:00 +0300

    implement qMRI metrics extraction

--- scripts/extract_normative_metrics.py
+++ scripts/extract_normative_metrics.py
@@ -4,6 +4,9 @@
 # Extract qMRI metrics (FA, MD, AD, RD, MTR, MTsat) from
 # individual ROI perlevel between C2 and C5
 #
+# Extracted metrics will be saved as *perlevel.csv files
+# in results/perlevel directory
+#
 # USAGE:
 # - parallel mode across multiple subjects (using SCT function sct_run_batch and extract_normative_metrics_wrapper.sh wrapper):
 #	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1 -script scripts/extract_normative_metrics_wrapper.sh
@@ -17,6 +20,16 @@
 import os
 import argparse
 import yaml
+
+# dict with qMRI metrics to process - metric: dir_location
+metrics_to_process = {
+    'FA': 'dwi',
+    'MD': 'dwi',
+    'AD': 'dwi',
+    'RD': 'dwi',
+    'MTR': 'anat',
+    'MTsat': 'anat',
+}
 
 # if no input yml file with labels/ROI to process is passed, then, following labels/ROIs will be used
 default_labels_to_process = {
@@ -83,16 +96,43 @@ def main():
         for key, value in labels_to_process.items():
             print('{}: {}'.format(key, value))
 
-    # put together -path_data and -sub and create subject_path; check if this dir exists
     if args.sub is not None and args.path_data is not None:
+        # put together -path_data and -sub and create subject_path; check if this dir exists
         subject_path = os.path.join(args.path_data, 'data_processed', args.sub)
         if not os.path.isdir(subject_path):
             raise FileNotFoundError("Path to subject dir {} is invalid.".format(subject_path))
+        # create dir perlevel in results folder where *perlevel.csv files will be saved
+        results_path = os.path.join(args.path_data, 'results', 'perlevel')
+        if not os.path.isdir(results_path):
+            os.mkdir(results_path)
 
-    print("Subject: {}".format(subject_path))
+    print("Processing subject: {}".format(subject_path))
 
-    for key in labels_to_process.keys():
-        print(key)
+    # list of labels/ROIs IDs to process
+    labels_to_process_list = list(labels_to_process.keys())
+
+
+    # Extract qMRI metrics perlevel
+    for metric, folder in metrics_to_process.items():       # loop across metrics
+        # dwi metrics (FA, MD, AD, RD)
+        if folder == 'dwi':
+            # nifti metric file
+            metric_fname = os.path.join(subject_path, folder, 'dti_' + metric + 'nii.gz')
+            # path to atlas
+            atlas_path = os.path.join(subject_path, folder, 'label', 'atlas')
+            # name of output csv file where perlevel values will be saved
+            csv_fname = os.path.join(results_path, 'DWI_' + metric + '_perlevel.csv')
+            #sct_extract_metric -i metric_fname -f atlas_path -l labels_to_process_list -vert 2:5 -perlevel 1 -o csv_fname -append 1
+        if folder == 'anat':
+            # nifti metric file
+            metric_fname = os.path.join(subject_path, folder, metric.lower)
+            # path to atlas
+            atlas_path = os.path.join(subject_path, folder, 'label_axT1w', 'atlas')
+            # path to vertfile
+            vertfile_fname = os.path.join(subject_path, folder, 'label_axT1w', 'template', 'PAM50_levels.nii.gz')
+            # name of output csv file where perlevel values will be saved
+            csv_fname = os.path.join(results_path, metric + '_perlevel.csv')
+            #sct_extract_metric -i metric_fname -f atlas_path -l labels_to_process_list -vert 2:5 -vertfile vertfile_fname -perlevel 1 -o csv_fname -append 1
 
 if __name__ == "__main__":
     main()
