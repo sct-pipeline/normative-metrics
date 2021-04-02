1e72481a96f8d8fb272657bb34c86dbd63ba5745
valosekj <jan.valosek@upol.cz>
2021-04-02 18:33:23 +0200

    Fix formatting.

--- scripts/extract_normative_metrics_wrapper.sh
+++ scripts/extract_normative_metrics_wrapper.sh
@@ -4,9 +4,11 @@
 #
 # USAGE:
 # - parallel mode across multiple subjects (using SCT sct_run_batch function and extract_normative_metrics_wrapper.sh wrapper):
-#	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1 -script scripts/extract_normative_metrics_wrapper.sh
-# - same as above, but with yml file containing labels to process(passed by -script-args option):
-#	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1 -script-args "-yml-file scripts/labels_to_process.yml" -script scripts/extract_normative_metrics_wrapper.sh
+#	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1
+#	    -script scripts/extract_normative_metrics_wrapper.sh
+# - same as above, but with yml file containing labels to process (passed by -script-args option):
+#	    sct_run_batch -jobs -1 -path-data ~/data-multi-subject/ -path-output ~/data-multi-subject_results -continue-on-error 1
+#	    -script-args "-yml-file scripts/labels_to_process.yml" -script scripts/extract_normative_metrics_wrapper.sh
 #
 # Author: Jan Valosek
 
@@ -18,8 +20,8 @@ INPUT_YML=$3
 
 # Run without yml file
 if [[ ${INPUT_YML} == "" ]];then
-	  # following command expects that this script is called from normative-metrics repo
-	  python scripts/extract_normative_metrics.py -sub ${SUBJECT} -path-data ${PATH_DATA}
+    # following command expects that this script is called from normative-metrics repo
+    python scripts/extract_normative_metrics.py -sub ${SUBJECT} -path-data ${PATH_DATA}
 # Run with passed yml file
 else
     # following command expect that this script is called from normative-metrics repo
